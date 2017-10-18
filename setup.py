0102b422f804af88c84b922b0ef393fbe8636e0b
Fernando Pelliccioni <fpelliccioni@gmail.com>
2017-10-18 11:04:49 -0300

    adding header file

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ from setuptools import setup
 from setuptools.extension import Extension
 
 PKG_NAME = 'cpuid_native'
-VERSION = '0.0.1'
+VERSION = '0.0.2'
 SYSTEM = sys.platform
 
 # ------------------------------------------------
